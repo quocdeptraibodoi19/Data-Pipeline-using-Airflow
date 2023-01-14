6caae3eaa2ff64135a0ef503ec043eacab6e5990
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2023-01-15 00:51:40 +0700

    adding the airflow dag code

--- airflow.py
+++ airflow.py
@@ -1,2 +1,29 @@
-import airflow
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
+from data_processing import run_elt
 
+default_args = {
+    "owner": "Nguyen Dinh Quoc",
+    "depends_on_past": False,
+    "start_date": datetime(year=2023, day=14, month=1),
+    "email": ["quocthogminhqtm@gmail.com"],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=4)
+}
+
+dag = DAG(
+    "twitter_etl_dag",
+    default_args=default_args,
+    description="This is the first time I deploy the airflow on AWS",
+)
+
+# The comma at the final parameter is just for the convention since it increases the maintanability and readability 
+run_etl = PythonOperator(
+    task_id = "running_twitter_etl",
+    python_callable= run_elt,
+    dag=dag,
+)
--- data_processing.py
+++ data_processing.py
@@ -2,23 +2,25 @@ from pandas import DataFrame
 import tweepy
 from Twitter_API_Key import access_token,access_token_secret,consumer_key,consumer_secret
 
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token,access_token_secret)
-api = tweepy.API(auth)
+def run_elt():
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token,access_token_secret)
+    api = tweepy.API(auth)
 
-public_tweets = api.user_timeline(screen_name="@baovtuber", include_rts=True, count=1000, tweet_mode ="extend")
+    public_tweets = api.user_timeline(screen_name="@baovtuber", include_rts=True, count=1000, tweet_mode ="extend")
 
-tweets= []
-for tweet in public_tweets:
-    refine_tweet = {
-        "user_name": tweet.user.screen_name,
-        "tweet": tweet.text,
-        "favorite number": tweet.favorite_count,
-        "retweeted number": tweet.retweet_count,
-        "language": tweet.lang,
-        "created_at": tweet.created_at
-    }
-    tweets.append(refine_tweet)
+    tweets= []
+    for tweet in public_tweets:
+        refine_tweet = {
+            "user_name": tweet.user.screen_name,
+            "tweet": tweet.text,
+            "favorite number": tweet.favorite_count,
+            "retweeted number": tweet.retweet_count,
+            "language": tweet.lang,
+            "created_at": tweet.created_at
+        }
+        tweets.append(refine_tweet)
 
-df = DataFrame(tweets)
-df.to_csv("./twitter_data.csv",index=False)
+    df = DataFrame(tweets)
+    df.to_csv("s3://my-test-airflow-bucket/twitter_data.csv",index=False)
+run_elt()
